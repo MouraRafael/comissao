9425ad81070fe46429df6e329f937a959f7b741c
Rafael Moura <rafael.g.moura@hotmail.com>
2022-10-06 16:53:17 -0300

    update: prepracao ambiente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CalculaComissaoComponent } from './calculaComissao/components/calcula-comissao/calcula-comissao.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalculaComissaoComponent
   ],
   imports: [
     BrowserModule
